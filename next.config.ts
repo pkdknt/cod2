36a7a9d0659c6c072a93521da52aba2ab59b1949
pkdknt <marketing.nhontamclinic@gmail.com>
2026-07-09 08:44:50 +0700

    Add redirect from tra-cuu-nhanh to cskh-tiem-chung

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.180', '192.168.1.180:3000', 'localhost:3000']
+  allowedDevOrigins: ['192.168.1.180', '192.168.1.180:3000', 'localhost:3000'],
+  async redirects() {
+    return [
+      {
+        source: '/tra-cuu-nhanh',
+        destination: '/cskh-tiem-chung',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
